61ca2788ece0e6638152d40334682a2af6775a7d
Prakhar Singh <prakharsingh11111@gmail.com>
2020-10-05 15:27:24 +0530

    Payment Gateway

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
@@ -16,5 +15,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express=require('express');
 const mongoose=require('mongoose');
 const cookieSession=require('cookie-session');
 const authRoutes=require('./routes/authRoutes');
+const billingRoutes=require('./routes/billingRoutes');
 const keys=require('./config/keys');
 const passport = require('passport');
+const bodyParser=require('body-parser');
+const path=require('path');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app=express();
+
+app.use(bodyParser.json());
 
 app.use(
     cookieSession({
@@ -22,6 +27,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 authRoutes(app);
+billingRoutes(app);
+
+if(process.env.NODE_ENV==='production'){
+//Express will serve up production assets(main.js, main.css)
+    app.use(express.static('client/build'));
+//Express will serve up index.html file if unrecognized route
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 const PORT=process.env.PORT || 5000;
 
