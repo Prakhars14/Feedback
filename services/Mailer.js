83e584fe5a7b7cd9e068ecc60d2f34ccf378282e
Prakhar Singh <prakharsingh11111@gmail.com>
2020-10-09 00:46:11 +0530

    SendGrid Mail Setup

---
+++ services/Mailer.js
@@ -0,0 +1,14 @@
+const sendgrid=require('sendgrid');
+const helper=sendgrid.mail;
+const keys=require('../config/keys');
+
+class Mailer extends helper.Mail {
+    constructor({subject, recipients}, content){
+        this.from_email=new helper.Email("prakharsingh11111@gmail.com");
+        this.subject=subject;
+        this.body=new helper.Content('text/html', content);
+        this.recipients=this.formatAddresses(recipients);
+    }
+}
+
+module.exports=Mailer;
