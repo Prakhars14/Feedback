99edd19cf93b7d0d589f50cbd2baf6ceb46ea471
Prakhar Singh <prakharsingh11111@gmail.com>
2021-06-15 07:35:39 +0530

    Chnages

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const cookieSession = require("cookie-session");
 const authRoutes = require("./routes/authRoutes");
 const billingRoutes = require("./routes/billingRoutes");
 const surveyRoutes = require("./routes/surveyRoutes");
 const passport = require("passport");
 const bodyParser = require("body-parser");
-require("dotenv").config();
+
 const path = require("path");
 require("./models/User");
 require("./models/Survey");
 require("./services/passport");
-console.log(process.env.NODE_ENV, "prakhar");
+console.log(process.env.MONGO_URI, "prakhar");
 mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
@@ -32,14 +33,12 @@ authRoutes(app);
 billingRoutes(app);
 surveyRoutes(app);
 
-if (process.env.NODE_ENV === "production") {
-  //Express will serve up production assets(main.js, main.css)
-  app.use(express.static("client/build"));
-  //Express will serve up index.html file if unrecognized route
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//Express will serve up production assets(main.js, main.css)
+app.use(express.static("client/build"));
+//Express will serve up index.html file if unrecognized route
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
