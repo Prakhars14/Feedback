8e7a783bc5b7cfbea8962637a10929e3fffcbf09
Prakhar Singh <prakharsingh11111@gmail.com>
2021-06-15 07:38:41 +0530

    Chnages

---
+++ client/build/precache-manifest.bd71e54d13b5b0ff392563611e4313b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfeee6d1f1b7d12db8067ebea32d0ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46c8d85e3e1652ca04fd",
+    "url": "/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "46c8d85e3e1652ca04fd",
+    "url": "/static/js/2.8c96cf79.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.8c96cf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96a9c2c88a242658f4c",
+    "url": "/static/js/main.86f75574.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
